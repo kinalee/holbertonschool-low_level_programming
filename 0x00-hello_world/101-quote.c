50c918655e43530951807d4b11b09eec6d725b48
Kina Lee <kina.lee@holbertonschool.com>
2016-10-12 23:08:38 +0000

    I am so glad I am donegit add 101-quote.c git add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,6 @@
 
 int main(void)
 {
-
-	perror("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
