27a34eb9e5f09a5ed6e6304fa03d513d4a28be87
Kina Lee <kina.lee@holbertonschool.com>
2016-10-12 18:24:35 +0000

    Betty style edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(){
-  printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+int main()
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
   
-  return 0;
+	return 0;
 }
