7d819c71e251e98e51af5d5224a3a90aab6ab41f
Kina Lee <kina.lee@holbertonschool.com>
2016-10-12 20:10:27 +0000

    comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - the main function
+ *
+ * Description: It is the main function."
+ * Return: Returned 0
+ */
+
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - the main function
+ *
+ * Description: It is the main function."
+ * Return: Returned 0
+ */
+
 int main(void)
 {
 	char charSize;
