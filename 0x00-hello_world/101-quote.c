bf22a4760303691cf8e248df92523cadded78de6
Kina Lee <kina.lee@holbertonschool.com>
2016-10-12 20:52:43 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Description: It is the main function."
+ * Return: Returned 0
+ */
+
+int main(void)
+{
+
+	perror("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
