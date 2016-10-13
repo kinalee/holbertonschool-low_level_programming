7f4c46439fd6c298fccf1b4109c9c4af03d3ac91
Kina Lee <kina.lee@holbertonschool.com>
2016-10-13 22:17:15 +0000

    It's suspicious but I am giving it a shot.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main and essential function to run this program
+ *
+ * Description: display 0-9
+ * Return: retunrs 0
+ */
+
+int main(void)
+{
+
+	printf("012345678\n");
+	return (0);
+
+}
