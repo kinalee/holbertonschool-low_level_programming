43b894bc04803ca270a6ad3a05936a312b320f76
Kina Lee <kina.lee@holbertonschool.com>
2016-10-13 23:25:33 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - the main and essential function to run this program
+ *
+ * Description: printing hexadecial number pattern from 0-f
+ * Return: retunrs 0
+ */
+
+int main(void)
+{
+
+	int d;
+	char h;
+
+	d = 0;
+	h = 'a';
+
+	while (d < 10)
+	{
+		putchar(d + '0');
+		++d;
+	}
+
+	while ( h < 'g')
+	{
+		putchar(h);
+		++h;
+	}
+
+	putchar('\n');
+	return (0);
+}
