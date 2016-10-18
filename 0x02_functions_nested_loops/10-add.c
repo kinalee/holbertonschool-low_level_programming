22d6b20d742e11b891180a1563baf326109d08e9
Kina Lee <kina.lee@holbertonschool.com>
2016-10-18 10:20:59 +0000

    a+b

---
+++ 0x02_functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - add two given integers and returns the sum
+ * @a: given integer 1/tested integer 1 
+ * @b: given integer 2/tested integer 2
+ * Return: result of the fuction
+ */
+
+int add(int a, int b)
+{
+
+	int sum;
+
+	sum = a + b;
+	return (sum);
+
+}
