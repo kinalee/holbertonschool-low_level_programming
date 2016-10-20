103f03c5bab432f1ad0888a5a790a71f2fcf58e6
Kina Lee <kina.lee@holbertonschool.com>
2016-10-20 07:20:56 +0000

    fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long f1, f2, n1, n2, p1, p2, i;
+	
+	n1 = 0;
+	n2 = 1;
+	p1 = 0;
+	p2 = 0;
+	i = 1;
+	
+	while (i < 98)
+	{
+
+		f1 = p1 + n1;
+		f2 = p2 + n2;
+
+		if (f2 > 999999999999999999)
+		{
+			++f1;
+			f2 = f2 % 1000000000000000000;
+		}
+
+		p2 = n2;
+		n2 = f2;
+
+		p1 = n1;
+		n1 = f1;
+
+		if (f1 != 0)
+		{
+			printf("%ld", f1);
+
+			if (i != 97)
+			{
+				printf("%018ld, ", f2);
+			}
+			else
+				printf("%018ld", f2);
+		}
+		else
+		{
+			printf("%ld, ", f2);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
