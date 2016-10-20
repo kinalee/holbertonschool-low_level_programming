2600ba5f0ade79d0a743542d034c34eb945c551a
Kina Lee <kina.lee@holbertonschool.com>
2016-10-20 20:29:39 +0000

    negative goes to the last

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -4,41 +4,41 @@
  * print_number - print given integers
  * @n: given/tested integer
  *
- * Description: Only used _putchar to print
+ * Description: Only use _putchar to print
  * Return: no return
  */
 
 void print_number(int n)
 {
-
-	if (n < 0)
-	{
-		n = n * (-1);
-		_putchar('-');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-
-	else if (n >= 0 && n < 10)
+	if (n >= 0 && n < 10)
 	{
 		_putchar(n + '0');
 	}
-	else if (n >= 10 && n < 100)
+	if (n >= 10 && n < 100)
 	{
 		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-	else if (n >= 100 && n < 1000)
+	if (n >= 100 && n < 1000)
 	{
 		_putchar(n / 100 + '0');
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-	else if (n >= 1000 && n < 10000)
+	if (n >= 1000 && n < 10000)
 	{
 		_putchar(n / 1000 + '0');
 		_putchar((n / 100) % 10 + '0');
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
+
 	}
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+
 }
