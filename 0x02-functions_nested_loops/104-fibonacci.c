46ad9688eb5be36633b0ef24ae8858b1e6c8ea9e
Kina Lee <kina.lee@holbertonschool.com>
2016-10-20 07:29:33 +0000

    betty whitespace fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers, 
- *\starting with 1 and 2
+ * main - finds and prints the first 98 Fibonacci numbers,
+ *\ starting with 1 and 2
  *
  * Description: not allowed to use long long or malloc
  * Return: returns 0
