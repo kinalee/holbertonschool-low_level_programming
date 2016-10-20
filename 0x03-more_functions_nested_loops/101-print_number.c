72a00c0f74a78bdccc197644d617efa7f3ea4a80
Kina Lee <kina.lee@holbertonschool.com>
2016-10-20 20:15:16 +0000

    print numbers

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * print_number - print given integers
+ * @n: given/tested integer
+ *
+ * Description: Only used _putchar to print
+ * Return: no return
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+
+	else if (n >= 0 && n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10 && n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
