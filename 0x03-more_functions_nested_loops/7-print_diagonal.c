ffdf45522b243334fd757cc86833e8e80f01a90d
Kina Lee <kina.lee@holbertonschool.com>
2016-10-20 10:29:25 +0000

    diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: Where n is the number of times the character \ should be printed
+ *
+ * Description: If n is 0 or less, the function should only print a \n
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		int j = 1;
+
+		while (j <= i)
+		{
+			_putchar(' ');
+			++j;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		++i;
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
