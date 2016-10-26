6ee00e1d10d406b08da65f52adbb87601f404178
Kina Lee <kina.lee@holbertonschool.com>
2016-10-26 01:57:47 +0000

    betty fixed

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * print_array - prints n elements of an array of integers,
+ *\followed by a new line
+ * @a: given array
+ * @n:the number of elements of the array to be printed
+ *
+ * Return: returns nothing.
+ */
+
 void print_array(int *a, int n)
 {
 
