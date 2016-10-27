ab80577d849ede2cbe38d3a6a5a74ce392cfa339
Kina Lee <kina.lee@holbertonschool.com>
2016-10-27 07:14:14 +0000

    100

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+/**
+ * _atoi - convert a string to an integer
+ * @s : given character pointer
+ *
+ * Return: returns nothing
+ */
+
+int _atoi(char *s)
+{
+	int i, j, neg, integer;
+	char s2[50];
+
+	i = j = neg = integer = 0;
+
+	while (!(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+		{
+			++neg;
+		}
+		++i;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		s2[j] = s[i];
+		++j;
+		++i;
+	}
+
+	s2[j] = '\0';
+	j = 0;
+
+	while (s2[j] != '\0')
+	{
+		integer = integer * 10 + (s2[j] - '0');
+		++j;
+	}
+	if (neg % 2 != 0)
+	{
+		integer *= -1;
+	}
+	return (integer);
+}
