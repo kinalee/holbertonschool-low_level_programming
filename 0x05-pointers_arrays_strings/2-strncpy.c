50804493c6796e9e65102f28cfb49a516266e679
Kina Lee <kina.lee@holbertonschool.com>
2016-10-27 21:59:33 +0000

    2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -19,9 +19,17 @@ char *_strncpy(char *dest, char *src, int n)
 		++i;
 	}
 
-	if (i == n && n == '\0')
+	while (src[i] == '\0')
 	{
-		dest[i] = '\0';
+		if (i == n)
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+			++i;
+		}
 	}
 
 	return (dest);
