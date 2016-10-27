7ccfc55d48a947035b4ac997d413659d50cf2de5
Kina Lee <kina.lee@holbertonschool.com>
2016-10-27 20:48:32 +0000

    1

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,51 @@
+/**
+ * _strlen - returns the length of a string
+ * @n: given character/string  pointer
+ *
+ * Return: returns the length of the string
+ */
+
+int _strlen(char *n)
+{
+	int l = 0;
+
+	while (n[l] != '\0')
+	{
+		++l;
+	}
+
+
+	return (l);
+}
+
+/**
+ * _strncat - concatenates two strings, using at most n bytes from src
+ * @dest: first string pointer
+ * @src: second string pointer
+ * @n: less than n numbers of digits will be printed
+ *
+ * Return: returns concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, l;
+
+	l = _strlen(dest);
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[l] = src[i];
+		++i;
+		++l;
+	}
+
+	if (i == n && n == '\0')
+	{
+		dest[l] = src[i];
+
+	}
+
+	return (dest);
+}
