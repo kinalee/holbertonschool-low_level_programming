7e3f6418b21964c41ffd61c978c5597cd3d3cb2b
Kina Lee <kina.lee@holbertonschool.com>
2016-10-27 21:12:10 +0000

    2

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+/**
+ * _strncpy - copies a string
+ * @dest: first string pointer
+ * @src: second string pointer
+ * @n: less than n numbers of digits will be copied
+ *
+ * Return: returns copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+
+	if (i == n && src[i] == '\0')
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+
+}
