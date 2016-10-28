6ad379d8923a21ab510ad0e0f71b065d0b792bd1
Kina Lee <kina.lee@holbertonschool.com>
2016-10-28 01:11:45 +0000

    when encounters null char

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,6 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
 	i = 0;
 
 	while (i < n)
@@ -18,6 +17,10 @@ char *_strncpy(char *dest, char *src, int n)
 		if (src[i] != '\0')
 		{
 			dest[i] = src[i];
+		}
+		else if (src[i] == '\0')
+		{
+			break;
 		}
 		else
 		{
