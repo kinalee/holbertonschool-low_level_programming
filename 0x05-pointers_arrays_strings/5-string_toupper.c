65c81e1c70cd1cd85bdbb115baa7ef9f9b756c4c
Kina Lee <kina.lee@holbertonschool.com>
2016-10-28 03:30:49 +0000

    5

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: given string/character
+ *
+ * Return: returns "uppercased" string
+ */
+
+
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+
+		++i;
+	}
+
+	return (str);
+}
