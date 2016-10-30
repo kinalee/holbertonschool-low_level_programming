dfdbb80c00a3f3f83400c612886f7fd4c92cdd3d
Kina Lee <kina.lee@holbertonschool.com>
2016-10-30 19:41:02 +0000

    103

---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_buffer - prints a buffer
+ * @b: given string
+ * @size: functions prints the content of size bytes of the buffer pointed by b
+ *
+ * Return: returns nothing
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+
+	i = j = 0;
+
+	while (i < size)
+	{
+		printf("%08x: ", b[i]);
+		k = 0;
+		while (k < 5)
+		{
+			if (j < size - 1)
+			{
+			printf("%02x%02x ", b[j], b[j + 1]);
+			}
+			else
+			{
+				printf("     ");
+			}
+			j = j + 2;
+			++k;
+		}
+		k = 0;
+		while (k < 10 && i < size)
+		{
+			if (isprint(b[i]))
+			{
+				printf("%c", b[i]);
+			}
+			else
+			{
+				printf(".");
+			}
+			++k;
+			++i;
+		}
+		printf("\n");
+	}
+}
