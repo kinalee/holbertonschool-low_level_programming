77bb552adbebc07f303bcb82763666e56ec84330
Kina Lee <kina.lee@holbertonschool.com>
2016-11-01 06:57:24 +0000

    9

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: given pointer of pointer of a string
+ * @to: given pointer of pointer of a string 2
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
