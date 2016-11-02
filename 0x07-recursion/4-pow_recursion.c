f9a4e96f139213404c6961bae666d66e8aa010c9
Kina Lee <kina.lee@holbertonschool.com>
2016-11-02 23:38:41 +0000

    4

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: exponent
+ *
+ * Return: returns the value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+
+	}
+
+}
