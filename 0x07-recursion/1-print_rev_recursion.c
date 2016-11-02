9acecd75b07504d90e8cd00d5684e27066e1c565
Kina Lee <kina.lee@holbertonschool.com>
2016-11-02 21:05:19 +0000

    1

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints a string, in reverse
+ * @s: given pointer to a string
+ *
+ * Return: returns nothing
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
