24ee547e43a75d1a7daed30977625785116ee770
Kina Lee <kina.lee@holbertonschool.com>
2016-11-10 01:13:54 +0000

    betty

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -18,7 +18,6 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-
 	ary = malloc(height * sizeof(int *));
 
 	if (ary == NULL)
@@ -26,32 +25,26 @@ int **alloc_grid(int width, int height)
 		free(ary);
 		return (NULL);
 	}
-
 	while (i < height)
 	{
 		ary[i] = malloc(width * sizeof(int));
 
 		if (ary[i] == NULL)
 		{
-			while (0 <= i)
+			while (i >= 0)
 			{
 				free(ary[i]);
 				--i;
 			}
-
 			free(ary);
 		}
-
 		j = 0;
-
 		while (j < width)
 		{
 			ary[i][j] = 0;
 			++j;
 		}
-
 		++i;
 	}
-
 	return (ary);
 }
