cdd280ffd929d3ee5af82a5100902f9b8b04d8d8
Kina Lee <kina.lee@holbertonschool.com>
2016-11-10 00:40:31 +0000

    betty

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -20,9 +20,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-
 	i = j = 0;
-
 	while (s1[i] != '\0')
 	{
 		++i;
@@ -32,18 +30,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		++j;
 	}
-
 	++j;
-
 	concat = malloc((i + j) * sizeof(char));
-
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
-
 	i = j = 0;
-
 	while (s1[i] != '\0')
 	{
 		concat[i] = s1[i];
@@ -56,8 +47,6 @@ char *str_concat(char *s1, char *s2)
 		++i;
 		++j;
 	}
-
 	concat[i] = '\0';
-
 	return (concat);
 }
