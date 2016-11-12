f3f2e36648bd96155b9206f5f93c8709f63d2cb6
Kina Lee <kina.lee@holbertonschool.com>
2016-11-12 04:48:52 +0000

    ptr == NULL

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (realloc == NULL)
 		return (NULL);
 
-	if (new_size > old_size)
+	if (new_size > old_size && ptr != NULL)
 	{
 		for (i = 0; i < new_size - old_size; ++i)
 			realloc[i] = temp[i];
