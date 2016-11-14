368e888a1231172213df9c7d14587fdd26062128
Kina Lee <kina.lee@holbertonschool.com>
2016-11-14 23:04:25 +0000

    3, 4

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,3 +1,5 @@
-#ifndef ABS
+#ifndef ABSOLUTE_H
+#define ABSOLUTE_H
+
 #define ABS(x) (x < 0 ? (x * (-1)) : (x))
 #endif
---
+++ 0x0C-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef SUM_NUMS_H
+#define SUM_NUMS_H
+
+#define SUM(x, y) (x + y)
+#endif
