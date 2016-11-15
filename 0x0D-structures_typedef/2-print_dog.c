d2b6a77ebfe4f3cec4cde53f2d2da8a733fa8a12
Kina Lee <kina.lee@holbertonschool.com>
2016-11-15 05:41:35 +0000

    nulls

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,7 +10,18 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
