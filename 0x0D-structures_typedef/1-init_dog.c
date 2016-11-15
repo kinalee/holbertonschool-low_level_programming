f608664933afc3aadceabcac73318523e0df125b
Kina Lee <kina.lee@holbertonschool.com>
2016-11-15 03:14:07 +0000

    1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type
+ * @d: structure variable of dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: returns nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
