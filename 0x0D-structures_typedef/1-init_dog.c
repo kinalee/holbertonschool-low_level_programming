78ff469feefefd001440be0abf4af68e5a79c14e
Kina Lee <kina.lee@holbertonschool.com>
2016-11-15 05:29:16 +0000

    1

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -18,18 +18,22 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	for (nl = 0; name[nl] != '\0'; ++nl)
 		;
 
-	d->name = malloc(nl * sizeof(char));
+	d->name = malloc((nl + 1) * sizeof(char));
 
 	for (nl = 0; name[nl] != '\0'; ++nl)
 		d->name[nl] = name[nl];
+
+	d->name[nl] = '\0';
 
 	d->age = age;
 
 	for (ol = 0; owner[ol] != '\0'; ++ol)
 		;
 
-	d->owner = malloc(ol * sizeof(char));
+	d->owner = malloc((ol + 1) * sizeof(char));
 
 	for (ol = 0; owner[ol] != '\0'; ++ol)
 		d->owner[ol] = owner[ol];
+
+	d->owner[ol] = '\0';
 }
