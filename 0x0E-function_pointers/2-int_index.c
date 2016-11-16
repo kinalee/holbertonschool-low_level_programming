7be8fa803902db8afce8b320215f5b0884061586
Kina Lee <kina.lee@holbertonschool.com>
2016-11-16 21:48:10 +0000

    2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  *int_index - function that searches for an integer
  *@array: given integer array
@@ -13,6 +15,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
+		return (-1);
+
+	if (array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; ++i)
