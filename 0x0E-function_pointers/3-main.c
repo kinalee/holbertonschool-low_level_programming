6ebea1017ffce3a8eb55cb69c1903eadf0a2c6f6
Kina Lee <kina.lee@holbertonschool.com>
2016-11-17 19:42:30 +0000

    null char

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 	op = argv[2][0];
 
 	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%' &&
-	    argv[2] != NULL)
+	    op != '\0')
 	{
 		printf("Error\n");
 		exit(99);
