342be8ede1bdfabcd03cd24ec6895236891ef164
Kina Lee <kina.lee@holbertonschool.com>
2016-11-20 03:47:30 +0000

    3

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -13,5 +13,21 @@ void print_strings(const char *separator, const unsigned int n, ...);
 /**3.function that prints anything */
 void print_all(const char * const format, ...);
 
-#endif
+/**
+ *struct print_types - Struct print_types
+ *
+ *@c: given character
+ *@i: given integer
+ *@f: given float
+ *@s: given string
+ */
 
+typedef struct print_types
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+} p_t;
+
+#endif
