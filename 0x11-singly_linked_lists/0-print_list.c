5dbaa6a17e6369c5399b9d2ccf143525f160c428
Kina Lee <kina.lee@holbertonschool.com>
2016-11-29 06:38:06 +0000

    betty and order

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -4,22 +4,20 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: linked list contains a pointer to a char variable and
- *\unsigned int variable 
+ *\unsigned int variable
  *
- * Retrun: number of nodes
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-        int i = 0;
+	int i = 0;
 
-        while (h != NULL)
-        {
-                printf("[%u] %s\n", h->len, h->str);
-                h = h->next;
-                ++i;
-        }
-
-        return (i);
-
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		++i;
+	}
+	return (i);
 }
