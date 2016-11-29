a7edbb6aabebfb00b76afd3a9133f1d9d766d88e
Kina Lee <kina.lee@holbertonschool.com>
2016-11-29 11:43:19 +0000

    bs

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -2,11 +2,10 @@
 
 /**
  * print_first - prints something before the main function is executed
- *
  * Return: returns nothing
  */
-
 void print_first(void) __attribute__ ((constructor));
+
 void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
