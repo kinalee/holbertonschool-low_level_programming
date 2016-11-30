ace9856a122b18528fbb98e34927b31db509855e
Kina Lee <kina.lee@holbertonschool.com>
2016-11-30 21:57:50 +0000

    5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "stdlib.h"
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the given list
+ * @head: pointer to a pointer of a list
+ * Return: returns nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
+		free(temp);
+		temp = *head;
+	}
+}
