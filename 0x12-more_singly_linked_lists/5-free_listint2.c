bd76031f3d280acd811f06da34e23f2ed3fc8dd3
Kina Lee <kina.lee@holbertonschool.com>
2016-12-01 12:20:40 +0000

    when head == NULL

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
+	if (head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		temp = *head;
 	}
 
 	*head = NULL;
