f8042f1d1ad8b04279f5f67121b62cd28eea7b15
Kina Lee <kina.lee@holbertonschool.com>
2016-12-01 15:57:39 +0000

    typo..

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	unsigned int i = 0;
 
 	if (head == NULL)
-		return;
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 
