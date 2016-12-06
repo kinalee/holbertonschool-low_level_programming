f920b0cfc1b2608dd121bc7806479cf2585172de
Kina Lee <kina.lee@holbertonschool.com>
2016-12-06 21:46:22 +0000

    flag

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * print_binary - prints the binary representation of a number
  * @n: given long int
@@ -7,17 +7,33 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int power;
+	signed long int size;
+	int flag = 0;
+	char c;
 
-	for (power = 1; (power << 1) <= n; power <<= 1)
-		;
+	size = sizeof(unsigned long int) * 8 - 1;
 
-	while (power >= 1)
+	if (n == 0)
 	{
-		if ((power & n) != 0)
-			_putchar('1');
+		_putchar('0');
+		return;
+	}
+
+	while (size >= 0)
+	{
+		c = (n >> size) & 1;
+
+		if (flag == 1)
+			_putchar(c + '0');
 		else
-			_putchar('0');
-		power >>= 1;
+		{
+			if (c == 1)
+			{
+				_putchar(c + '0');
+				flag = 1;
+			}
+		}
+
+		size -= 1;
 	}
 }
