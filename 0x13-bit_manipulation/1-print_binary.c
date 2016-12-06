d57fac3eb9b1f65a8043aa44d7bbd5660512af61
Kina Lee <kina.lee@holbertonschool.com>
2016-12-06 05:09:06 +0000

    1

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: given long int
+ * Return: returns nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int power;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (power = 1; (power * 2) <= n; power *= 2)
+		;
+
+	while (power > 0)
+	{
+		if ((power & n) > 0)
+			_putchar('1');
+		else
+			_putchar('0');
+		power = power >> 1;
+	}
+}
