09c69c076784859e7498bfc9f68122d12e129937
Kina Lee <kina.lee@holbertonschool.com>
2016-12-08 04:36:40 +0000

    another oc

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 
 	if (fd == -1)
 		return (-1);
+
+	if (text_content == NULL)
+		return (1);
 
 	i = 0;
 
