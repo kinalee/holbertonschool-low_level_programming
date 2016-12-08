25e522c3fcede1fe2e9f0e0e3be56f7975792b84
Kina Lee <kina.lee@holbertonschool.com>
2016-12-08 05:04:03 +0000

    index......

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -14,15 +14,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
 	fd = open(filename, O_RDWR | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content != '\0'; ++i)
+	if (text_content == NULL)
+		return (1);
+
+	for (i = 0; text_content[i] != '\0'; ++i)
 		;
 
 	w = write(fd, text_content, i);
