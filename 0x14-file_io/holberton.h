f599f8b301d0fd6247c760125c7f37d824447c52
Kina Lee <kina.lee@holbertonschool.com>
2016-12-08 04:01:24 +0000

    _putchar

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -6,6 +6,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
