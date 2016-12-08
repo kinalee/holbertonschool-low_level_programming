c45b0d0bc224ec28ae627f06f92bb928a06d440f
Kina Lee <kina.lee@holbertonschool.com>
2016-12-08 04:16:48 +0000

    file do not exist

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -24,6 +24,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 		w = write(fd, text_content, i);
+	else
+		return (-1);
 
 	if (w == -1)
 		return (-1);
