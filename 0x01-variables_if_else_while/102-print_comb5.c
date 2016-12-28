c1b686d9a7ce1f1c0a9aee9d10a0a70ac8132acb
Kina Lee <kina.lee@holbertonschool.com>
2016-12-28 10:05:33 +0000

    cleaned up

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,36 @@
 #include <stdio.h>
 
 /**
- *main - the main and essential function to run this program
- *
- *Description: prints all possible combinations of two two-digit numbers.
+ *main - prints all possible combinations of two two-digit numbers
  *Return: returns 0
  */
-
 int main(void)
 {
-	int a, b, c, d, x, y;
+	int i, j;
 
-	a = 0;
-	d = b = c = a;
-	while (a < 10 && b < 10 && c < 10 && d < 10)
+	i = 0;
+	while (i < 100)
 	{
-		x = (a * 10) + (b * 1);
-		y = (c * 10) + (d * 1);
-		if (x < y)
+		j = 0;
+		while (j < 100)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(' ');
-			putchar(c + '0');
-			putchar(d + '0');
-			if (a != 9 || b != 8 || c != 9 || d != 9)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			++j;
 		}
-			++d;
-			if (d == 10)
-			{
-				d = 0;
-				++c;
-			}
-			if (c == 10)
-			{
-				c = 0;
-				++b;
-			}
-			if (b == 10)
-			{
-				b = 0;
-				++a;
-			}
+		++i;
 	}
 	putchar('\n');
 	return (0);
