74760c401d9fe012db5ff510bbce0aaaed6ca056
Kina Lee <kina.lee@holbertonschool.com>
2016-12-29 22:44:43 +0000

    return

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,37 +9,37 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n <= 15 || n >= 0)
+	if (n > 15 || n < 0)
+		return;
+
+	i = 0;
+	while (i <= n)
 	{
-		i = 0;
-		while (i <= n)
+		j = 0;
+		while (j <= n)
 		{
-			j = 0;
-			while (j <= n)
+			k = i * j;
+			if (k > 99)
+				_putchar(k / 100 + '0');
+
+			if (k > 9)
+				_putchar((k / 10) % 10 + '0');
+
+			_putchar(k % 10 + '0');
+
+			if (j < n)
 			{
-				k = i * j;
-				if (k > 99)
-					_putchar(k / 100 + '0');
-
-				if (k > 9)
-					_putchar((k / 10) % 10 + '0');
-
-				_putchar(k % 10 + '0');
-
-				if (j < n)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+				if (i * (j + 1) < 10)
 					_putchar(' ');
-					if (i * (j + 1) < 10)
-						_putchar(' ');
 
-					if (i * (j + 1) < 100)
-						_putchar(' ');
-				}
-				++j;
+				if (i * (j + 1) < 100)
+					_putchar(' ');
 			}
-			_putchar('\n');
-			++i;
+			++j;
 		}
+		_putchar('\n');
+		++i;
 	}
 }
