03c326ddc1e8d9eb69cc8836f22b43f7a2091a2f
Kina Lee <kina.lee@holbertonschool.com>
2017-02-05 12:05:35 +0000

    hmm

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -10,21 +10,21 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i, flag;
 	hash_node_t *tmp;
 
+	if (ht == NULL)
+		return;
+
 	flag = 0;
 	printf("{");
-	if (ht != NULL)
+	for (i = 0; i < ht->size; ++i)
 	{
-		for (i = 0; i < ht->size; ++i)
+		tmp = ht->array[i];
+		if (tmp != NULL)
 		{
-			tmp = ht->array[i];
-			if (tmp != NULL)
-			{
-				if (flag == 1)
-					printf(", ");
-				printf("'%s': '%s'", tmp->key, tmp->value);
-				flag = 1;
-				tmp = tmp->next;
-			}
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
