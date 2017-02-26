f994120bd00f6c69aec242b75640e66048eb459c
Kina Lee <kina.lee@holbertonschool.com>
2017-02-26 11:21:41 +0000

    quick sort

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -28,5 +28,6 @@ void print_list(const listint_t *list);
 /* tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 
 #endif
